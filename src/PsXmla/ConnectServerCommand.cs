e59ba0b530b0b1ff13874871159eb9d970487333
Steffen <skampmann@munichre.com>
2021-10-14 12:12:05 +0200

    Added credential auth

--- src/PsXmla/ConnectServerCommand.cs
+++ src/PsXmla/ConnectServerCommand.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Management.Automation;
+using System.Net;
+using System.Security;
 using System.Text.RegularExpressions;
 using Microsoft.AnalysisServices.Tabular;
 using Microsoft.Azure.Services.AppAuthentication;
@@ -72,6 +74,26 @@ namespace PsXmla
         [Parameter()]
         public SwitchParameter InteractiveAuthentication;
 
+        [Parameter()]
+        public string Username {
+            get { return Credential.UserName; } 
+            set { Credential.UserName = value; } 
+        }
+
+        [Parameter()]
+        public SecureString Password
+        {
+            get { return Credential.SecurePassword; }
+            set { Credential.SecurePassword = value; }
+        }
+
+        [Parameter()]
+        public NetworkCredential Credential { get; set; }
+
+        string Protocol { get; set; }
+
+        private Server Connection { get; set; }  = new Server();
+
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
@@ -79,70 +101,51 @@ namespace PsXmla
             switch (ParameterSetName)
             {
                 case "PowerBi":
-                    DataSource = $"powerbi://api.powerbi.com/v1.0/{PowerBiTenant}/{PowerBiWorkspace}";
-                    {
-                        ConnectionString = $"DataSource={DataSource}";
-                        if (!string.IsNullOrEmpty(PowerBiDataset))
-                        {
-                            ConnectionString += $";initial catalog={PowerBiDataset}";
-                            WriteVerbose("Connect by PowerBiWorkspace and PowerBiDataset.");
-                        }
-                        else
-                        {
-
-                            WriteVerbose("Connect by PowerBiWorkspace.");
-                        }
-                    }
+                    SetDataSourceForPowerBiParameters();
+                    SetConnectionStringByPowerBiParameters();
                     break;
 
                 case "Properties":
-                    {
-                        ConnectionString = $"DataSource={DataSource}";
-                        if (!string.IsNullOrEmpty(InitialCatalog))
-                        {
-                            ConnectionString += $";initial catalog={InitialCatalog}";
-                            WriteVerbose("Connect by DataSource and InitialCatalog.");
-                        }
-                        else
-                        {
-
-                            WriteVerbose("Connect by DataSource.");
-                        }
-                    }
+                    SetConnectionStringByServerParameters();
                     break;
 
                 case "ConnectionString":
-                    {
-                        var dataSourceRegex = new Regex(@"Data.?Source=""?(.+)""?;?");
-                        var match = dataSourceRegex.Match(ConnectionString);
-                        if (match.Success)
-                        {
-                            DataSource = match.Groups[1].Value;
-                            WriteVerbose($"ConnectionString contains DataSource {DataSource}");
-                        }
-                        else
-                        {
-                            WriteWarning($"ConnectionString lacks a DataSource");
-                        }
-
-                        WriteVerbose("Connect by connection string.");
-                    }
+                    SetDataSourceByConnectionString();
                     break;
 
                 default:
                     throw new NotImplementedException($"ParameterSetName {ParameterSetName}");
             }
 
-            {
-                string protocol = null;
-                var protocolRegex = new Regex(@"(\w+)://");
-                var match = protocolRegex.Match(DataSource);
-                if (match.Success)
-                {
-                    protocol = match.Groups[1].Value;
-                }
+            SetProtocolByDataSource();
+            AddAuthenticationToConnectionString();
+            ConnectByConnectionString();
+            WriteObject(Connection);
+        }
 
-                switch (protocol)
+        private void ConnectByConnectionString()
+        {
+            Connection.Connect(ConnectionString);
+        }
+
+        private void AddAuthenticationToConnectionString()
+        {
+            if (!string.IsNullOrEmpty(Username))
+            {
+                ConnectionString += $";Username={Username}";
+
+                if (Password.Length > 0)
+                {
+                    ConnectionString += $";Password={new NetworkCredential(string.Empty, Password).Password}";
+                    WriteVerbose("Authenticate via Username and Password");
+                } else
+                {
+                    WriteVerbose("Authenticate via Username");
+                }
+            }
+            else
+            {
+                switch (Protocol)
                 {
                     case "powerbi":
                         if (!InteractiveAuthentication.IsPresent)
@@ -153,18 +156,74 @@ namespace PsXmla
                                 AccessToken = new AzureServiceTokenProvider().GetAccessTokenAsync("https://analysis.windows.net/powerbi/api.default").Result;
                             }
                             ConnectionString += $";Password={AccessToken}";
+                            WriteVerbose("Authenticate via AccessToken");
                         }
                         break;
 
                     default:
-                        throw new NotSupportedException($"Protocol {protocol} is not supported");
+                        throw new NotSupportedException($"Protocol {Protocol} is not supported");
                 }
             }
+        }
 
-            var connection = new Server();
-            WriteVerbose($"Connect to { ConnectionString }");
-            connection.Connect(ConnectionString);
-            WriteObject(connection);
+        private void SetProtocolByDataSource()
+        {
+            var protocolRegex = new Regex(@"(\w+)://");
+            var match = protocolRegex.Match(DataSource);
+            if (match.Success)
+            {
+                Protocol = match.Groups[1].Value;
+            }
+        }
+
+        private void SetDataSourceByConnectionString()
+        {
+            var dataSourceRegex = new Regex(@"Data.?Source=""?(.+)""?;?");
+            var match = dataSourceRegex.Match(ConnectionString);
+            if (match.Success)
+            {
+                DataSource = match.Groups[1].Value;
+                WriteVerbose($"ConnectionString contains DataSource {DataSource}");
+            }
+            else
+            {
+                WriteWarning($"ConnectionString lacks a DataSource");
+            }
+        }
+
+        private void SetConnectionStringByServerParameters()
+        {
+            ConnectionString = $"DataSource={DataSource}";
+            if (!string.IsNullOrEmpty(InitialCatalog))
+            {
+                ConnectionString += $";initial catalog={InitialCatalog}";
+                WriteVerbose("Connect by DataSource and InitialCatalog.");
+            }
+            else
+            {
+
+                WriteVerbose("Connect by DataSource.");
+            }
+        }
+
+        private void SetConnectionStringByPowerBiParameters()
+        {
+            ConnectionString = $"DataSource={DataSource}";
+            if (!string.IsNullOrEmpty(PowerBiDataset))
+            {
+                ConnectionString += $";initial catalog={PowerBiDataset}";
+                WriteVerbose("Connect by PowerBiWorkspace and PowerBiDataset.");
+            }
+            else
+            {
+
+                WriteVerbose("Connect by PowerBiWorkspace.");
+            }
+        }
+
+        private void SetDataSourceForPowerBiParameters()
+        {
+            DataSource = $"powerbi://api.powerbi.com/v1.0/{PowerBiTenant}/{PowerBiWorkspace}";
         }
     }
 }
