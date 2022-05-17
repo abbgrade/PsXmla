096193abee17bc3e5c1ee61f00a9a39e743f862b
Steffen <skampmann@munichre.com>
2022-05-17 10:31:16 +0200

    refactored invoke command

--- src/PsXmla/InvokeCommandCommand.cs
+++ src/PsXmla/InvokeCommandCommand.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AnalysisServices;
-using Microsoft.AnalysisServices.Tabular;
 using Newtonsoft.Json;
 using Polly;
 using System;
@@ -14,19 +13,20 @@ namespace PsXmla
         [Parameter(
             Mandatory = true,
             ValueFromPipeline = true,
-            ValueFromPipelineByPropertyName = true)]
+            ValueFromPipelineByPropertyName = true
+        )]
         [ValidateNotNullOrEmpty()]
         public string Command { get; set; }
 
         [Parameter(
-            Mandatory = true,
-            ValueFromPipelineByPropertyName = true)]
+            Mandatory = true
+        )]
         [ValidateNotNullOrEmpty()]
         public Microsoft.AnalysisServices.Tabular.Server Connection { get; set; }
 
         [Parameter(
-            Mandatory = false,
-            ValueFromPipelineByPropertyName = true)]
+            Mandatory = false
+        )]
         [ValidateNotNullOrEmpty()]
         public int? MaxRetryAttempts { get; set; }
 
@@ -63,23 +63,35 @@ namespace PsXmla
         private void ExecuteCommand()
         {
 
-            bool success = true;
             XmlaResultCollection results = null;
+            bool success = true;
             try
             {
+                WriteVerbose(Command);
                 results = Connection.Execute(Command);
             }
             catch (ResponseFormatException ex)
             {
-                WriteWarning(ex.InnerException.Message);
-                if (!IgnoreResponseFormatException.IsPresent)
-                    throw ex;
-            }
 
+                if (IgnoreResponseFormatException.IsPresent)
+                {
+                    WriteWarning(ex.InnerException.Message);
+                }
+                else
+                {
+                    WriteError(new ErrorRecord(
+                        exception: ex,
+                        errorId: "Error",
+                        errorCategory: ErrorCategory.OperationStopped,
+                        targetObject: Command));
+                    success = false;
+                }
+            }
             var exceptions = new List<Exception>();
 
             if (results != null)
             {
+                WriteVerbose($"Execution returned {results.Count} results.");
                 foreach (XmlaResult result in results)
                 {
                     if (result.Value.Length > 0)
@@ -105,22 +117,27 @@ namespace PsXmla
                                             {
                                                 case "DMTS_MonikerWithUnboundDataSources":
                                                     exceptions.Add(new Exception("Datasource not bound to gateway"));
+                                                    success = false;
                                                     break;
 
                                                 case "DM_GWPipeline_Gateway_DataSourceAccessError":
                                                     exceptions.Add(new Exception("Database not available or accessable"));
+                                                    success = false;
                                                     break;
 
                                                 case "DM_GWPipeline_Gateway_InvalidObjectNameException":
                                                     exceptions.Add(new Exception("Invalid object name in datasource"));
+                                                    success = false;
                                                     break;
 
                                                 case "DM_GWPipeline_Gateway_CanceledError":
                                                     exceptions.Add(new Exception("Operation canceled"));
+                                                    success = false;
                                                     break;
 
                                                 default:
                                                     exceptions.Add(new Exception(code.Value));
+                                                    success = false;
                                                     break;
                                             }
                                             break;
@@ -128,6 +145,7 @@ namespace PsXmla
                                         else
                                         {
                                             exceptions.Add(new Exception($"{message.Source}: {message.Description}"));
+                                            success = false;
                                         }
                                     }
                                 }
@@ -137,8 +155,6 @@ namespace PsXmla
                                 }
                                 break;
                         }
-
-                        success = false;
                     }
                 }
             }
