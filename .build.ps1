45e789f7d7fba8402532789c967bace3e3ded576
Steffen Kampmann <steffen.kampmann@gmail.com>
2022-07-08 08:29:32 +0000

    install psbuild tasks

--- .build.ps1
+++ .build.ps1
@@ -7,10 +7,17 @@ param(
 	[ValidateSet('Debug', 'Release')]
 	[string] $Configuration = 'Debug',
 
-	[string] $NuGetApiKey = $env:nuget_apikey
+	[string] $NuGetApiKey = $env:nuget_apikey,
+
+	# Version suffix to prereleases
+	[int] $BuildNumber
 )
 
+$ModuleName = 'PsXmla'
+
 . $PSScriptRoot\tasks\Build.Tasks.ps1
+. $PSScriptRoot\tasks\Dependencies.Tasks.ps1
+. $PSScriptRoot\tasks\PsBuild.Tasks.ps1
 
 # Synopsis: Default task.
 task . Build
